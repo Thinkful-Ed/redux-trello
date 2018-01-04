c3f34964b05cf0f7666a78d2f011e294de5dd79d
Joe Turner <joe@oampo.co.uk>
2018-01-04 17:46:25 +0000

    Prettier

--- src/components/board.js
+++ src/components/board.js
@@ -14,11 +14,11 @@ export class Board extends React.Component {
     }
 
     render() {
-        const lists = this.props.lists.map((list, index) =>
-        <li className="list-wrapper" key={index}>
-            <List index={index} {...list} />
-        </li>
-        );
+        const lists = this.props.lists.map((list, index) => (
+            <li className="list-wrapper" key={index}>
+                <List index={index} {...list} />
+            </li>
+        ));
 
         return (
             <div className="board">
@@ -46,4 +46,3 @@ const mapStateToProps = state => ({
 });
 
 export default connect(mapStateToProps)(Board);
-
